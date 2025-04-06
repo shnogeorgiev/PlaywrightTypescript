035f1c9d49877d97a123cd1dc1b7c2fde8a530b5
PC <shnogeorgiev@gmail.com>
2025-04-06 05:02:44 +0300

    test azure remote commit

--- src/pages/homePage.ts
+++ src/pages/homePage.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { BasePage } from '../support/basePage';
 import { Navigation } from '../support/components/navigation';
 
